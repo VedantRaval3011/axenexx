06d749ce38c3a4ca97b38025c8f2363e30b95e3a
VedantRaval3011 <vedantraval30@gmail.com>
2024-10-02 11:16:46 +0530

    Features scroll animation completed

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 import { ReactLenis } from "lenis/dist/lenis-react";
 import Hero from "./components/Hero";
 import Brands from "./components/Brands";
+import Features from "./components/Features";
 
 export default function Home() {
   return (
@@ -9,7 +10,7 @@ export default function Home() {
     <ReactLenis root>
       <Hero />
       <Brands />
-      
+      <Features />
     </ReactLenis>
   </>
   );
