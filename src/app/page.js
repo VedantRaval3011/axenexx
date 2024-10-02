42f4701bf1f8937e19797bde67c8653583c6cbe5
VedantRaval3011 <vedantraval30@gmail.com>
2024-10-02 10:57:45 +0530

    Added all required images

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 "use client";
 import { ReactLenis } from "lenis/dist/lenis-react";
 import Hero from "./components/Hero";
+import Brands from "./components/Brands";
 
 export default function Home() {
   return (
     <>
     <ReactLenis root>
       <Hero />
+      <Brands />
       
     </ReactLenis>
   </>
